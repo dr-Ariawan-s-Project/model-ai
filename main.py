80df996ebcfd74f5d140b7128bd5f814cba5daf5
rachmankamil <rachmankamil24@gmail.com>
2023-09-19 12:59:10 +0700

    add train and evaluate data.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tensorflow import keras
+from sklearn.model_selection import train_test_split
 
 data = np.array([
     [1,1,1,1],
@@ -16,6 +17,9 @@ result = np.array([
     0
 ])
 
+data_train, data_test, result_train, result_test = train_test_split(data, result, test_size=0.2, random_state=42)
+
+
 model = keras.Sequential([
     keras.layers.Dense(5, activation='relu', input_shape=(4,)),
     keras.layers.Dense(1, activation='sigmoid')
@@ -23,7 +27,7 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
 
-model.fit(data, result, epochs=100, batch_size=32, validation_split=0.2)
+model.fit(data_train, result_train, epochs=100, batch_size=32, validation_split=0.2)
 
 new_data = np.array([[1,0,1,0]])
 predicted_result = model.predict(new_data)
@@ -39,6 +43,6 @@ for layer in model.layers:
         print(f'Weights:\n{weights}')
         print(f'Biases:\n{biases}\n')
 
-accuracy = model.evaluate(data, result)
-print(accuracy)
-# print(f'Accuracy: {accuracy * 100:.2f}%')
+test_loss, test_accuracy = model.evaluate(data_test, result_test)
+print(f'Test accuracy: {test_accuracy * 100:.2f}%')
+print(f'Test loss: {test_loss:.2f}%')
